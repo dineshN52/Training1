9d014e67462fedd8abbf6b23561e7a2fe1093964
dineshN52 <dinesh.n@metamation.com>
2023-08-21 14:13:50 +0530

    variable i is initialized in loop itself and indentation updated

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
-﻿namespace Training {
+﻿using system
+namespace fibonocci {
    internal class Program {
-         public static void Main (string[] args) {
-            int n1 = 0, n2 = 1, n3, i, number;
-            Console.Write ("Enter the number of elements: ");
-            number = int.Parse (Console.ReadLine ());
-            Console.Write (n1 + " " + n2 + " "); //printing 0 and 1    
-            for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
-            {
-               n3 = n1 + n2;
-               Console.Write (n3 + " ");
-               n1 = n2;
-               n2 = n3;
-            }
+      public static void Main (string[] args) {
+         int n1 = 0, n2 = 1, n3, number;// n1 & n2 were the input, n3 & number were temporary variable
+         Console.Write("Enter the number of elements:");
+         number = int.Parse (Console.ReadLine ());
+         Console.WriteLine(n1 + " " + n2 + " "); //printing 0 and 1    
+         for (int i = 2; i < number; ++i) { //loop starts from 2 because 0 and 1 are already printed
+            n3 = n1 + n2;
+            Console.WriteLine(n3 + " ");
+            n1 = n2;
+            n2 = n3;
          }
+         Console.ReadKey ();
+      }
    }
 }
