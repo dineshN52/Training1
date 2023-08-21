f14a408f54553020c8dbe7947144488feb63c34a
dineshN52 <dinesh.n@metamation.com>
2023-08-21 11:02:10 +0530

    New change-Instead of char int was initialized and compared

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ namespace Palindrome {
    class Program {
       static bool IsPalindrome (string input) {
          input = input.ToLower (); // Convert the input to lowercase for case-insensitive comparison
-         char left = input[0];
-         char right = input[input.Length - 1];
+         int left = 0;
+         int right = input.Length - 1;
          while (left < right) {
             if (input[left] != input[right])
                return false;
@@ -20,6 +20,7 @@ namespace Palindrome {
             Console.WriteLine ("Given input is a palindrome.");
          else
             Console.WriteLine ("Given input is not a palindrome.");
+         Console.ReadKey ();
       }
    }
 }
