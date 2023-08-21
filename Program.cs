2dc3a38377ef87ffd9e5ea66b558098b23fae645
dineshN52 <dinesh.n@metamation.com>
2023-08-21 15:46:29 +0530

    Change requested are commited

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
-﻿using system
+﻿using System;
 namespace Training {
    internal class Program {
       public static void Main (string[] args) {
-         int n1 = 0, n2 = 1, n3, i, number;
+         int n1 = 0, n2 = 1, n3, number; //n1 & n2 are input, n3 and number are temporary variable
          Console.Write ("Enter the number of elements: ");
          number = int.Parse (Console.ReadLine ());
          Console.Write (n1 + " " + n2 + " "); //printing 0 and 1    
-         for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
-         {
+         for (int i = 2; i < number; ++i) { //loop starts from 2 because 0 and 1 are already printed    
             n3 = n1 + n2;
             Console.Write (n3 + " ");
             n1 = n2;
             n2 = n3;
          }
+         Console.ReadKey ();
       }
    }
 }
