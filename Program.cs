c8619b3793a9a13b52a87667dd473bdcee71ee59
dineshN52 <dinesh.n@metamation.com>
2023-08-21 08:48:31 +0530

    Palindrome checker code is updated

--- Program.cs
+++ Program.cs
@@ -1,27 +1,25 @@
 ﻿using System;
-namespace Training {
-   namespace Prime {
-      class Program {
-         static bool IsPrime (int num) {
-            if (num <= 1)
+namespace Palindrome {
+   class Program {
+      static bool IsPalindrome (string input) {
+         input = input.ToLower (); // Convert the input to lowercase for case-insensitive comparison
+         char left = input[0];
+         char right = input[input.Length - 1];
+         while (left < right) {
+            if (input[left] != input[right])
                return false;
-            else if (num == 2 & num == 3)
-               return true;
-            for (int i = num - 1; i > 1; i--) {
-               if (num % i == 0)
-                  return false;
-            }
-            return true;
+            left++;
+            right--;
          }
-         static void Main (string[] args) {
-            Console.Write ("Enter a number: ");
-            int number = int.Parse (Console.ReadLine ());
-
-            if (IsPrime (number))
-               Console.WriteLine ($"{number} is a prime number.");
-            else
-               Console.WriteLine ($"{number} is not a prime number.");
-         }
+         return true;
+      }
+      static void Main (string[] args) {
+         Console.Write ("Enter a string: ");
+         string input = Console.ReadLine ();
+         if (IsPalindrome (input))
+            Console.WriteLine ("Given input is a palindrome.");
+         else
+            Console.WriteLine ("Given input is not a palindrome.");
       }
    }
 }
