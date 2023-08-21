11dd67fe645763bbddcfa8344b2b38fe7151f9ed
dineshN52 <dinesh.n@metamation.com>
2023-08-21 15:28:42 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -2,17 +2,17 @@
 namespace Training {
    internal class Program {
       public static void Main (string[] args) {
-         int n1 = 0, n2 = 1, n3, number;// n1 & n2 were the input, n3 & number were temporary variable
-         Console.Write("Enter the number of elements:");
+         int n1 = 0, n2 = 1, n3, i, number;
+         Console.Write ("Enter the number of elements: ");
          number = int.Parse (Console.ReadLine ());
-         Console.WriteLine(n1 + " " + n2 + " "); //printing 0 and 1    
-         for (int i = 2; i < number; ++i) { //loop starts from 2 because 0 and 1 are already printed
+         Console.Write (n1 + " " + n2 + " "); //printing 0 and 1    
+         for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+         {
             n3 = n1 + n2;
-            Console.WriteLine(n3 + " ");
+            Console.Write (n3 + " ");
             n1 = n2;
             n2 = n3;
          }
-         Console.ReadKey ();
       }
    }
 }
