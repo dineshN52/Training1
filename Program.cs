bc00b76e3751ef476d82a56b397d6d5db149880f
dineshN52 <dinesh.n@metamation.com>
2023-08-22 10:29:40 +0530

    Changes like removal of unnecessary braces,variable declaration & Start a function class with caps

--- Program.cs
+++ Program.cs
@@ -2,31 +2,27 @@
 using System.Runtime.CompilerServices;
 namespace LCMGCD {
    internal class Program {
-      static int gcd (int a, int b) {
+      static int Gcd (int a, int b) {
          while (b != 0) {
             int temp = b;
             b = a % b; a = temp;
          }
          return a;
       }
-      static int lcm (int a, int b) {
-         return (a * b) / gcd (a, b);
+      static int Lcm (int a, int b) {
+         return (a * b) / Gcd (a, b);
       }
       static void Main (string[] args) {
          Console.WriteLine ("Enter the first number");
          int num1 = int.Parse (Console.ReadLine ());
-         if (num1 <= 0) {
+         if (num1 <= 0)
             Console.WriteLine ("Enter a Positive integer");
-         }
          Console.WriteLine ("Enter the other number");
          int num2 = int.Parse (Console.ReadLine ());
-         if (num2 <= 0) {
+         if (num2 <= 0)
             Console.WriteLine ("Enter a Positive integer");
-         }
-         int LCM = lcm (num1, num2);
-         int GCD = gcd (num1, num2);
-         Console.WriteLine ($"LCM of {num1} and {num2} is {LCM}");
-         Console.WriteLine ($"GCD of {num1} and {num2} is {GCD}");
+         Console.WriteLine ($"LCM of {num1} and {num2} is {Lcm (num1, num2)}");
+         Console.WriteLine ($"GCD of {num1} and {num2} is {Gcd (num1, num2)}");
          Console.ReadKey ();
       }
    }
