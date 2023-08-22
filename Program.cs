465044b21eed361adb83e09650b0712bcd2ecc5f
dineshN52 <dinesh.n@metamation.com>
2023-08-22 10:02:12 +0530

    Changed the description of variables declared

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@
 namespace Training {
    internal class Program {
       public static void Main (string[] args) {
-         int n1 = 0, n2 = 1, n3, number; //n1 & n2 are input, n3 and number are temporary variable
+         int n1 = 0, n2 = 1, n3, number; //n1 & n2 are initial numbers of fibonocci series
          Console.Write ("Enter the number of elements: ");
-         number = int.Parse (Console.ReadLine ());
+         number = int.Parse (Console.ReadLine ());//number is the count of elements in fiboncci series
          Console.Write (n1 + " " + n2 + " "); //printing 0 and 1    
          for (int i = 2; i < number; ++i) { //loop starts from 2 because 0 and 1 are already printed    
-            n3 = n1 + n2;
+            n3 = n1 + n2;//n3 is the temporary variable which stores added consecutive numbers
             Console.Write (n3 + " ");
             n1 = n2;
             n2 = n3;
