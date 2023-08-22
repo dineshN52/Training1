6e265f1376c53a8debe4c6eb463d43b1844e8505
dineshN52 <dinesh.n@metamation.com>
2023-08-22 11:41:34 +0530

    Loop count reduced and updated

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ namespace Prime {
       static bool IsPrime (int b) {
          if (b <= 1)
             return false;
-         for (int i = b - 1; i > 1;i--)
-            if (b % i == 0)
+         for (int i = 2; i * i <= b; i++) 
+            if (b % i == 0) 
                return false;
          return true;
       }
