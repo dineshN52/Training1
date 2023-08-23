f44d0083a0f2eb9b8a3062348ded3e3c9ed994c0
dineshN52 <dinesh.n@metamation.com>
2023-08-23 10:02:01 +0530

    Concat function is replaced with interpolation

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Prime {
       static void Main (string[] args) {
          Console.Write ("Enter a number:");
          int a = int.Parse (Console.ReadLine ());
-         Console.WriteLine (string.Concat ($"{a} is" , IsPrime (a) ? " a prime number" : " not a prime number"));
+         Console.WriteLine ($"{a} is{(IsPrime (a) ? " a prime number" : " not a prime number")}");
          Console.ReadKey ();
       }
    }
