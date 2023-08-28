09273c85309cfb1e3437ce2cc64b46f7588f67ef
dineshN52 <dinesh.n@metamation.com>
2023-08-28 17:39:52 +0530

    Requested changes are updated

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 namespace multitable {
    class Program {
       static void Main (string[] args) {
-         int maxTable = 10, tableSize = 10;
-         for (int table = 1; table <= maxTable; table++) {
-            for (int multiplier = 1; multiplier <= tableSize; multiplier++) {
-               int result = table * multiplier;
-               Console.WriteLine ($"{table} * {multiplier} = {result}");
+         int maxT = 10, Size = 10;
+         for (int table = 1; table <= maxT; table++) {
+            for (int a = 1; a <= Size; a ++) {
+               int r = table * a;
+               Console.WriteLine ($"{table} * {a} = {r}");
             }
             Console.WriteLine ();
          }
