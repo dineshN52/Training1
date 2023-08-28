c8313a2457aa944a564c0fa6299d316d2118a115
dineshN52 <dinesh.n@metamation.com>
2023-08-28 09:42:38 +0530

    New function for hex & binary conversion updated

--- Program.cs
+++ Program.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 namespace Numconv {
-   internal class Program {
+   class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Enter the decimal number:");
-         int deci = int.Parse (Console.ReadLine ());
-         string hex = Convert.ToString (deci, 16); // Convert to hexadecimal
-         string binary = Convert.ToString (deci, 2);// Convert to binary
-         Console.WriteLine ($"The Hexadecimal output:{hex}");
-         Console.WriteLine ($"The binary output:{binary}");
+         Console.WriteLine ("Enter the number");// Convert integer to hexadecimal string
+         int intValue = int.Parse (Console.ReadLine ());
+         string hexValue = IntToHex (intValue);
+         Console.WriteLine ($"Integer {intValue} in hexadecimal: 0x{hexValue}");
+         string BiValueFromInt = IntToBi (intValue);
+         Console.WriteLine ($"Integer {intValue} as Binary: {BiValueFromInt}");
          Console.ReadKey ();
+      }
+      static string IntToHex (int value) {
+         string hex = "";
+         while (value > 0) {
+            int remainder = value % 16;
+            hex = GetHexDigit (remainder) + hex;
+            value /= 16;
+         }
+         return hex;
+      }
+      static string IntToBi (int value) {
+         string binary = "";
+         while (value > 0) {
+            int remainder = value % 2;
+            binary = remainder + binary;
+            value /= 2;
+         }
+         if (binary == "")
+            binary = "0";  // Special case for input value 0
+         return binary;
+      }
+      static char GetHexDigit (int value) {
+         return value < 10 ? (char)('0' + value) : (char)('A' + (value - 10));
       }
    }
 }
