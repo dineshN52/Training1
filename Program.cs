e301ce3671f074b94bf3b1a2f1419d75f410c37d
dineshN52 <dinesh.n@metamation.com>
2023-08-30 12:02:02 +0530

    Unrequired variables removed

--- Program.cs
+++ Program.cs
@@ -4,15 +4,14 @@ namespace reversenumber {
       static void Main (string[] args) {
          Console.Write ("Enter a number: ");
          int num = int.Parse (Console.ReadLine ());
-         int reversedNumber = ReverseNumber (num);
-         Console.WriteLine ($"Reversed number: {reversedNumber}");
+         Console.WriteLine ($"Reversed number: {ReverseNumber(num)}");
          Console.WriteLine (IsPalindrome (num) ? "The number is a palindrome." : "The number is not a palindrome");
+         Console.ReadKey ();
       }
       static int ReverseNumber (int a) { // reverse a number
          int reverse = 0;
          while (a > 0) {
-            int digit = a % 10;
-            reverse = reverse * 10 + digit;
+            reverse = reverse * 10 + a % 10;
             a /= 10;
          }
          return reverse;
