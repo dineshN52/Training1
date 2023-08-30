458047a4e6388d13baf7c6581bccdb1d05339eea
dineshN52 <dinesh.n@metamation.com>
2023-08-30 13:04:36 +0530

    Requested changes are made and updated

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@ namespace reversenumber {
       static void Main (string[] args) {
          Console.Write ("Enter a number: ");
          int num = int.Parse (Console.ReadLine ());
-         int b = ReverseNumber (num);
-         Console.WriteLine ($"Reversed number: {b}");
-         Console.WriteLine (b ==num ? "The number is a palindrome." : "The number is not a palindrome");
+         Console.WriteLine (ReverseNumber (num) == num ? "The number is a palindrome." : "The number is not a palindrome");
          Console.ReadKey ();
       }
       static int ReverseNumber (int a) { // reverse a number
