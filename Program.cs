f173e3191d477e144357ef4bde85e3f8107ac944
dineshN52 <dinesh.n@metamation.com>
2023-08-30 11:52:38 +0530

    Unrequired braces are removed and updated

--- Program.cs
+++ Program.cs
@@ -4,9 +4,8 @@ namespace multitable {
       static void Main (string[] args) {
          int maxT = 10, Size = 10;
          for (int table = 1; table <= maxT; table++) {
-            for (int a = 1; a <= Size; a ++) {
+            for (int a = 1; a <= Size; a ++) 
                Console.WriteLine ($"{table} * {a} = {table * a}");
-            }
             Console.WriteLine ();
          }
          Console.ReadKey ();
