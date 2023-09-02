90f8c1b9bba9f32e8116a96fa5c8b025df142299
dineshN52 <dinesh.n@metamation.com>
2023-09-02 15:50:44 +0530

    Isogram checker code is upadted

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿namespace Isogram {
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter the string: ");
+         string s = Console.ReadLine ();
+         Console.WriteLine (IsIsogram (s) ? "It is an isogram" : "It is not an isogram");
+         Console.ReadKey ();
+      }
+      static bool IsIsogram (string s) {
+         HashSet<char> seenCharacters = new HashSet<char> ();
+         foreach (char c in s) {
+            if (seenCharacters.Contains (c))
+               return false;
+            seenCharacters.Add (c);
+         }
+         return true;
+      }
+   }
 }
