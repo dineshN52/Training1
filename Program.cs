a27bdafde4ead2af70081402e7475fc1199924f3
dineshN52 <dinesh.n@metamation.com>
2023-09-05 09:29:57 +0530

    Changes are made and updated

--- Program.cs
+++ Program.cs
@@ -18,15 +18,13 @@ namespace chess_board {
                chessboard[row, col] = "\u00a0"; // Empty square      
          }
          Console.WriteLine ();
-         for (int i = 0; i < 8; i++) {
-            Console.Write (i == 0 ? "\u250c\u2500\u2500\u2500" : "\u252c\u2500\u2500\u2500");
-         }
+         for (int i = 0; i < 8; i++) 
+            Console.Write (i == 0 ? "\u250c\u2500\u2500\u2500" : "\u252c\u2500\u2500\u2500");        
          Console.WriteLine ("\u2510");
          for (int row = 0; row < boardSize; row++) {
             Console.Write ("\u2502");
-            for (int col = 0; col < boardSize; col++) {
-               Console.Write ($" {chessboard[row, col]} \u2502");
-            }
+            for (int col = 0; col < boardSize; col++) 
+               Console.Write ($" {chessboard[row, col]} \u2502");           
             Console.WriteLine ();
             if (row < boardSize - 1) {
                Console.Write ("\u251c");
@@ -38,9 +36,8 @@ namespace chess_board {
                Console.WriteLine ("\u2524");
             }
          }
-         for (int i = 0; i < 8; i++) {
-            Console.Write (i == 0 ? "\u2514\u2500\u2500\u2500" : "\u2534\u2500\u2500\u2500");
-         }
+         for (int i = 0; i < 8; i++) 
+            Console.Write (i == 0 ? "\u2514\u2500\u2500\u2500" : "\u2534\u2500\u2500\u2500");         
          Console.WriteLine ("\u2518");
          Console.ReadKey ();
       }
