be71662e77b2b1e1acab9a4cbb248acd3ce03fcc
dineshN52 <dinesh.n@metamation.com>
2023-09-05 10:02:40 +0530

    String interpolation included for space after each number

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace pascals {
       static void PascalsTriangle (int numRows) {
          for (int i = 0; i < numRows; i++) {
             int num = 1;
-            Console.Write (new string (' ', numRows - i - 1)); // Print leading spaces
+            Console.Write (new string(' ',numRows -i-1)); // Print leading spaces
             for (int j = 0; j <= i; j++) {
-               Console.Write (num + " ");
+               Console.Write ($"{num} ");
                num = num * (i - j) / (j + 1);
             }
             Console.WriteLine ();
