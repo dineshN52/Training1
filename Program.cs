43909fcc5af65e366d8f55b463fc136ade4bd8e5
dineshN52 <dinesh.n@metamation.com>
2023-09-12 11:08:03 +0530

    Number conversion game code updated

--- Program.cs
+++ Program.cs
@@ -1,78 +1,86 @@
 ﻿using System;
-using System.Linq.Expressions;
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 namespace NandR {
    class Program {
       static void Main (string[] args) {
          long n;
-         Console.Write("Enter the value:");
+         Console.WriteLine ("Enter the value:");
          n = long.Parse (Console.ReadLine ());
-         Console.WriteLine ("Number into words:");
-         Console.WriteLine (int_to_words (n));
+         Console.WriteLine ("Original integer value: " + n);
          Console.WriteLine ("Roman numerals of the said integer value:");
          Console.WriteLine (int_to_Roman (n));
+         Console.WriteLine ("Number into words:");
+         Console.WriteLine (IntToWords (n));
          Console.ReadKey ();
       }
-      public static string int_to_words (long n) {
-         string[] ones = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-         string[] teens = new string[] { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-         string[] tens = new string[] { "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
-         string[] mult = new string[] { "zero", "ten", "hundred", "thousand", "thousand", "million", "billion", "trillion", "hundred and" };
-         var intToW = new StringBuilder ();
-         int a;long b;
-         if (n == 0) 
-            return mult[0];       
-         a = n.ToString ().Length;
-         if (a == 13) {
-            long trillions = n / 1000000000000;
-            intToW.Append (int_to_words ((int)trillions)).Append (" ").Append (mult[7]);
-            b=n%1000000000000;
-            if (b > 0) 
-               intToW.Append (" ");           
+      public static string IntToWords (long n) {
+         string[] ones = new string[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+         string[] teens = new string[] { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+         string[] tens = new string[] { "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+         string[] mult = new string[] { "zero", "Ten", "Hundred", "Thousand", "Million", "Billion", "Trillion" };
+         StringBuilder intToW = new ();
+         int a = n.ToString ().Length;
+         if (a >= 13) {
+            HandleTrillion (n, mult, intToW);
+         } else if (a >= 10) {
+            HandleBillion (n, mult, intToW);
+         } else if (a >= 7) {
+            HandleMillion (n, mult, intToW);
+         } else if (a >= 4) {
+            HandleThousand (n, mult, intToW);
+         } else if (a == 3) {
+            HandleHundreds (n, intToW, ones, mult[2]);
+         } else if (a == 2) {
+            HandleTensAndTeens (n, intToW, ones, teens, tens);
+         } else if (a == 1) {
+            intToW.Append (ones[n - 1]);
          }
-         else if (a == 10) {
-            long billions = n / 1000000000;
-            intToW.Append (int_to_words ((int)billions)).Append (" ").Append (mult[6]);
-            n %= 1000000000;
-            if (n > 0) 
-               intToW.Append (" ");            
-         }
-         else if (a == 7) {
-            int millions = (int)n / 1000000;
-            intToW.Append (int_to_words (millions)).Append (" ").Append (mult[5]);
-            n %= 1000000;
-            if (n > 0) 
-               intToW.Append (" ");           
-         }
-         else if (a == 4) {
-            int thousands = (int)n / 1000;
-            intToW.Append (int_to_words (thousands)).Append (" ").Append (mult[3]);
-            n %= 1000;
-            if (n > 0) 
-              intToW.Append (" ");            
-         }
-         else if (n == 3) {
-            int hundreds = (int)n / 100;
-            intToW.Append (ones[hundreds - 1]).Append (" ").Append (mult[2]);
-            n %= 100;
-            if (n > 0) 
-               intToW.Append (" and ");           
-         }
-         else if (a == 2) {
-            if (n >= 20) {
-               int tensDigit = (int)n / 10, onesDigit = (int)n % 10;
-               intToW.Append (tens[tensDigit - 1]).Append (ones[onesDigit-1]);              
-            } else if (n >= 10 && n < 20) {
-               intToW.Append (teens[n - 10]);
-            }
-            //else if (n > 0)
-              // intToW.Append (int_to_words (n));
-         }
-         else if (a==1) {
-           intToW.Append (ones[n - 1]);
-         } 
          return intToW.ToString ();
+      }
+      private static void HandleTrillion (long n, string[] mult, StringBuilder intToW) {
+         long trillions = n / 1000000000000;
+         intToW.Append (IntToWords (trillions)).Append (" ").Append (mult[6]).Append (" ");
+         n %= 1000000000000;
+         if (n > 0)
+            intToW.Append (IntToWords (n)).Append (" ");
+      }
+      private static void HandleBillion (long n, string[] mult, StringBuilder intToW) {
+         long billions = n / 1000000000;
+         intToW.Append (IntToWords (billions)).Append (" ").Append (mult[5]).Append (" ");
+         n %= 1000000000;
+         if (n > 0)
+            intToW.Append (IntToWords (n)).Append (" ").Append (" ");
+      }
+      private static void HandleMillion (long n, string[] mult, StringBuilder intToW) {
+         long millions = n / 1000000;
+         intToW.Append (IntToWords (millions)).Append (" ").Append (mult[4]).Append (" ");
+         n %= 1000000;
+         if (n > 0)
+            intToW.Append (IntToWords (n)).Append (" ");
+      }
+      private static void HandleThousand (long n, string[] mult, StringBuilder intToW) {
+         int thousands = (int)n / 1000;
+         intToW.Append (IntToWords (thousands)).Append (" ").Append (mult[3]).Append (" ");
+         n %= 1000;
+         if (n > 0)
+            intToW.Append ("and ").Append (IntToWords (n));
+      }
+      private static void HandleHundreds (long n, StringBuilder intToW, string[] ones, string hundred) {
+         int hundreds = (int)n / 100;
+         intToW.Append (ones[hundreds - 1]).Append (" ").Append (hundred).Append (" ");
+         n %= 100;
+         if (n > 0)
+            intToW.Append (" and ").Append (IntToWords (n));
+      }
+      private static void HandleTensAndTeens (long n, StringBuilder intToW, string[] ones, string[] teens, string[] tens) {
+         if (n >= 20) {
+            int tensDigit = (int)n / 10, onesDigit = (int)n % 10;
+            intToW.Append (tens[tensDigit - 1]);
+            if (onesDigit > 0)
+               intToW.Append (" ").Append (ones[onesDigit - 1]);
+         } else if (n >= 10 && n < 20) {
+            intToW.Append (teens[n - 10]);
+         }
       }
       public static string int_to_Roman (long n) {
          string[] roman_symbol = { "MMM", "MM", "M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I" };
@@ -85,8 +93,8 @@ namespace NandR {
                roman_numerals.Append (roman_symbol[index_num]);
             } else
                index_num++;
-         } 
+         }
          return roman_numerals.ToString ();
       }
-   }   
+   }
 }
