37f39ecdea184fc41df8800850475dced84b1894
dineshN52 <dinesh.n@metamation.com>
2023-09-13 16:51:14 +0530

    Used Tryparse,Implemented triangle using 2D array

--- Program.cs
+++ Program.cs
@@ -3,17 +3,27 @@ namespace Pascals {
    class Program {
       static void Main (string[] args) {
          Console.Write ("Enter the number of rows: ");
-         int numRows = int.Parse (Console.ReadLine ());
-         PascalsTriangle (numRows);
+         uint numRows;
+         while (true) {
+            if (uint.TryParse (Console.ReadLine (), out numRows)) {
+               PascalsTriangle (numRows);
+               break;
+            } else {
+               Console.WriteLine ("Invalid input. Please enter a valid non negative integer");
+            }
+         }
          Console.ReadKey ();
       }
-      static void PascalsTriangle (int numRows) {
+      static void PascalsTriangle (uint numRows) {
+         int[,] triangle = new int[numRows, numRows];
          for (int i = 0; i < numRows; i++) {
-            int num = 1;
-            Console.Write (new string (' ', numRows - i - 1)); // Print leading spaces
+            Console.Write (new string (' ', (int)numRows - i - 1)); // Print leading spaces
             for (int j = 0; j <= i; j++) {
-               Console.Write ($"{num} ");
-               num = (num * (i - j)) / (j + 1);
+               if (j == 0 || j == i)
+                  triangle[i, j] = 1;
+               else
+                  triangle[i, j] = triangle[i - 1, j - 1] + triangle[i - 1, j];
+               Console.Write ($"{triangle[i, j]} "); // Fill the triangle with values
             }
             Console.WriteLine ();
          }
