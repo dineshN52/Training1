b1f8e6e000cb26c0628e8daf635d4f5630dee009
dineshN52 <dinesh.n@metamation.com>
2023-09-20 14:06:10 +0530

    String permutation code is added

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
-﻿namespace Training {
-   internal class Program {
+﻿using System;
+namespace StringPermutation {
+   class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         Console.Write ("Enter the string: ");
+         string input = Console.ReadLine ();
+         Console.WriteLine ("Permutations:");
+         Permute (input.ToCharArray (), 0, input.Length - 1);
+         Console.ReadKey ();
+      }
+      static void Permute (char[] arr, int startIndex, int endIndex) {
+         if (startIndex == endIndex) {
+            Console.WriteLine (new string (arr));
+         } else {
+            for (int i = startIndex; i <= endIndex; i++) {
+               Swap (ref arr[i], ref arr[startIndex]);
+               Permute (arr, startIndex + 1, endIndex);
+            }
+         }
+      }
+      static void Swap (ref char a, ref char b) {
+         char temp = a;
+         a = b;
+         b = temp;
       }
    }
 }
