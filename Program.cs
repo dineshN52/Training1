2924295f2cf0b4caf5224a7f5e713992aa98967b
dineshN52 <dinesh.n@metamation.com>
2023-09-20 12:55:21 +0530

    Main branch created

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.WriteLine ("Hello, World!");
+      }
+   }
 }
