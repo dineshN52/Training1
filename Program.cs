4185cdf9e31639e8abe5a1c9b609913932e5bb8c
Dinesh N <dinesh.n@metamation.com>
2023-10-04 11:46:33 +0530

    List was created insted of hashset

--- Program.cs
+++ Program.cs
@@ -13,19 +13,19 @@ namespace Isogram {
    /// <summary>Isogram checker</summary>
    internal class Program {
       #region Method-------------------------
-      /// <summary>Method gets the input string from user and passes into an IsIsogram method to check it isogram or not</summary>
+      /// <summary>Method gets the input from user and passes into an IsIsogram method to check it isogram or not</summary>
       /// <param name="args"></param>
       static void Main (string[] args) {
          Console.Write ("Enter the string: ");
          string s = Console.ReadLine ();
-         Console.WriteLine (IsIsogram (s) ? "It is an isogram" : "It is not an isogram");
+         Console.WriteLine ($"{s} is {(IsIsogram (s.ToLower ()) ? "an" : "not an")} isogram");
          Console.ReadKey ();
       }
-      /// <summary>This method checks for the repetition of letter in character and produce output as Isogram or not an isogram</summary>
-      /// <param name="s"></param>
-      /// <returns>It returns the boolean output of true or false for IsIsogarm function(If Isogram:True,Not Isogram:False)</returns>
+      /// <summary>This method checks for the repetition of letter in input & produce output as Isogram or not</summary>
+      /// <param name="s">String from user</param>
+      /// <returns>It returns boolean output of true or false for IsIsogarm function(If Isogram:True,Not Isogram:False)</returns>
       static bool IsIsogram (string s) {
-         HashSet<char> seenCharacters = new HashSet<char> ();
+         List<char> seenCharacters = new List<char> ();
          foreach (char c in s) {
             if (seenCharacters.Contains (c))
                return false;
