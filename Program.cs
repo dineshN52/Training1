ea6d2801d654db41cc88158f25edb11763d4e966
Dinesh N <dinesh.n@metamation.com>
2023-10-13 17:55:19 +0530

    Enumerator is prefixed with E,GetMax method removed and maximum values are returned in getmode method

--- Program.cs
+++ Program.cs
@@ -18,15 +18,14 @@ namespace NumberGuess {
       /// <param name="args"></param>
       static void Main (string[] args) {
          Console.Write ("Enter the mode: (E)asy, (M)edium, (H)ard :");
-         var mode = GetMode ();
-         int max = GetMax (mode);
-         Console.WriteLine ($"{mode}\nGuess & Enter the number between 1 and {max}");
+         int max = GetMode ();
+         Console.WriteLine ($"\nGuess & Enter the number between 1 and {max}");
          int secret = new Random ().Next (1, max + 1);
          for (int tries = 1; ; tries++) {
             Console.Write (">");
             if (int.TryParse (Console.ReadLine (), out int guess)) {
                var result = Checkguess (secret, guess);
-               if (result == Guess.Exact) {
+               if (result == EGuess.Exact) {
                   Console.WriteLine ($"you guessed correctly in {tries} tries");
                   break;
                } else
@@ -36,40 +35,37 @@ namespace NumberGuess {
          }
          Console.ReadKey ();
       }
-      /// <summary>Enumeration class for selecting the mode</summary>
-      enum Mode { Easy, Medium, Hard };
+
       /// <summary>Enumeration class for returning the guessed value</summary>
-      enum Guess { Exact, Low, High };
+      enum EGuess { Exact, Low, High };
+
       /// <summary>Method to select the mode</summary>
       /// <returns>Returns the mode selected by the user</returns>
-      static Mode GetMode () {
+      static int GetMode () {
          while (true) {
             var keyInfo = Console.ReadKey (intercept: true);
             switch (keyInfo.Key) {
-               case ConsoleKey.E: return Mode.Easy;
-               case ConsoleKey.M: return Mode.Medium;
-               case ConsoleKey.H: return Mode.Hard;
+               case ConsoleKey.E:
+                  Console.Write ("Easy");
+                  return 10;
+               case ConsoleKey.M:
+                  Console.Write ("Medium");
+                  return 100;
+               case ConsoleKey.H:
+                  Console.Write ("Hard");
+                  return 1000;
             }
          }
       }
-      /// <summary>Method to get maximum value top assign random number for input mode</summary>
-      /// <param name="mode"></param>
-      /// <returns>Returns the maximum value for respective mode</returns>
-      static int GetMax (Mode mode) {
-         switch (mode) {
-            case Mode.Easy: return 10;
-            case Mode.Medium: return 100;
-            default: return 1000;
-         }
-      }
+
       /// <summary>Method to check entered number is equivalent,lower or higher to random number</summary>
       /// <param name="secret"></param>
       /// <param name="guess"></param>
       /// <returns>It returns boolean answer for the guessing with secret number</returns>
-      static Guess Checkguess (int secret, int guess) {
-         if (guess == secret) return Guess.Exact;
-         if (guess < secret) return Guess.Low;
-         return Guess.High;
+      static EGuess Checkguess (int secret, int guess) {
+         if (guess == secret) return EGuess.Exact;
+         if (guess < secret) return EGuess.Low;
+         return EGuess.High;
       }
       #endregion
    }
