1d303570e778ea14fe17cb3b4c21ddf0d8f0b9dc
Dinesh N <dinesh.n@metamation.com>
2023-10-16 12:20:14 +0530

    writeline statment removed from swap method,necessary spaces added,condition in main method changed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿// ------------------------------------------------------------------------------------------
+﻿// -------------------------------------------------------------------------------------------
 // Training~A training program for new joinees at metamation,Batch-July 2023
 // Copyright(c) Metamation India
-// ------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------
 // Program.cs
 // Swap values of two variables
 // For given two variable input it swap their respective values in it
-// For Example,if A=90,B=45 as input, the output will be A=45,B=90 as their values are swaped
-// ------------------------------------------------------------------------------------------
+// For Example, if A=90,B=45 as input, the output will be A=45,B=90 as their values are swaped
+// -------------------------------------------------------------------------------------------
 namespace Swapnum {
    #region class Program-------------------------------
    /// <summary>Swap values of two variable</summary>
@@ -15,24 +15,24 @@ namespace Swapnum {
       /// <summary>Method gets valid integer input from the user and pass it to swap function</summary>
       /// <param name="args"></param>
       static void Main (string[] args) {
-         int a, b;
-         do {
-            Console.Write ("Enter the valid number for A:");
-         } while (!int.TryParse (Console.ReadLine (), out a));
-         do {
-            Console.Write ("Enter the valid number for B:");
-         } while (!int.TryParse (Console.ReadLine (), out b));
-         Console.WriteLine ($"Original values A={a} B={b}");
-         Swap (a, b);
+         Console.Write ("Enter two integer values separated by a space: ");
+         while (true) {
+            string[] s = Console.ReadLine ().Split (' ');
+            if (s.Length == 2 && int.TryParse (s[0], out int a) && int.TryParse (s[1], out int b)) {
+               Console.WriteLine ($"Original values A={a} B={b}");
+               Swap (ref a, ref b);
+               Console.WriteLine ($"Swaped values A={a} B={b}");
+               break;
+            } else
+               Console.WriteLine ("Invalid input. Enter integer values separated by a space");
+         }
          Console.ReadKey ();
       }
+
       /// <summary>Method takes two inputs and swap the values</summary>
       /// <param name="a">Input1</param>
       /// <param name="b">Input2</param>
-      static void Swap (int a, int b) {
-         (b, a) = (a, b);
-         Console.WriteLine ($"Swaped values A={a} B={b}");
-      }
+      static void Swap (ref int a, ref int b) => (b, a) = (a, b);
       #endregion
    }
    #endregion
