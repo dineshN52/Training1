3db09a27cd355ada65e06d106d09df96aa1fbf8d
Dinesh N <dinesh.n@metamation.com>
2023-10-16 09:40:45 +0530

    Spelling are corrected for abecedarian

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
-﻿// ---------------------------------------------------------------------------------------------------------------------------
+﻿// ---------------------------------------------------------------------------------------------------------------------------------
 // Training~A training program for new joinees at metamation,Batch-July 2023
 // Copyright(c) Metamation India
-// ---------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------
 // Program.cs
-// Longest Abecederian word
-// For given list of words,Find the longest abecedrian word and returns it
-// For Example,in word "Aegilops",Letters are arranged in alphabetical order so, it is abecederaian
-// The word Aim is also arranged in alphabetical order but it is smaller in length,so it returns Aegilops as LongestAbecederian
-// ----------------------------------------------------------------------------------------------------------------------------
+// Longest abecedarian word
+// For given list of words,Find the longest abecedarian word and returns it
+// For Example, in word "Aegilops",Letters are arranged in alphabetical order so, it is abecedarian
+// The word "Aim" is also arranged in alphabetical order but it is smaller in length,so it returns "Aegilops" as longest abecedarian
+// ---------------------------------------------------------------------------------------------------------------------------------
 using System;
-namespace Abecederain {
+namespace Abecedarian {
    #region Program-------------------------------------
-   /// <summary>Longest abecederian word</summary>
+   /// <summary>Longest abecedarian word</summary>
    class Program {
       #region Methods------------------------------------------
-      /// <summary>Method to check whether input has any abecederian word, if it has returns the longest word</summary>
+      /// <summary>Method to check whether input has any abecedarian word, if it has returns the longest word</summary>
       /// <param name="args"></param>
       static void Main (string[] args) {
          string[] words = { "Aegilops", "Aim", "Bill", "Lilly", };
@@ -26,7 +26,7 @@ namespace Abecederain {
 
       /// <summary>Method to find whether a word has letters arranged in alphabetical order</summary>
       /// <param name="word">Input words</param>
-      /// <returns>It returns true if word is abecederian,false if it not abecederian</returns>
+      /// <returns>It returns true if word is abecedarian,false if it not abecedarian</returns>
       static bool IsAbecedarian (string word) {
          word = word.ToLower ();
          for (int i = 1; i < word.Length; i++) {
@@ -36,9 +36,9 @@ namespace Abecederain {
          return true;
       }
 
-      /// <summary>Method to check the longest of all abecederian words</summary>
+      /// <summary>Method to check the longest of all abecedarian words</summary>
       /// <param name="words">Input words</param>
-      /// <returns>Returns the longest Abecederain word</returns>
+      /// <returns>Returns the longest abecedarain word</returns>
       static string LongAbecedarian (string[] words) {
          string longword = "";
          foreach (string word in words) {
