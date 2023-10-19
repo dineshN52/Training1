20f8c188a8f14ab3b412691fa60fdb495ab6f6ae
Dinesh N <dinesh.n@metamation.com>
2023-10-19 12:53:50 +0530

    Necessary spaces added,comment lines are corrected,unnecessary braces removed

--- Program.cs
+++ Program.cs
@@ -1,43 +1,45 @@
-﻿//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//Training~A training program for new joinees at metamation,Batch-July 2023
-//Copyright(c) Metamation India
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//Program.cs
-//NUMBERS TO WORDS & ROMAN NUMERALS
-//Ask the user to enter the number and convert it into equivalent english words and roman numerals
-//For Example,For user input of 78563: The output produced will be Roman numerals:MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMDLXIII
-//For the same number output produced for words conversion is "Seventy Eight thousand and Five hundred and Sixty Three
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+﻿// -------------------------------------------------------------------------------------------------------------
+// Training~A training program for new joinees at metamation,Batch-July 2023
+// Copyright(c) Metamation India
+// -------------------------------------------------------------------------------------------------------------
+// Program.cs
+// Number to words and roman numerals
+// Ask the user to enter the number and convert it into equivalent english words and roman numerals
+// For Example,For user input of 8547: the output produced will be roman numerals:MMMMMMMMDXLVII
+// For the same number output produced for words conversion is "Eight Thousand and Five Hundred and Forty Seven"
+// -------------------------------------------------------------------------------------------------------------
 using System;
 using System.Text;
 namespace NandR {
    #region Program------------------------------------------------------------------------
-   /// <summary>NUMBER TO WORDS & ROMAN</summary>
+   /// <summary>Number to words and roman numerals</summary>
    class Program {
       #region Methods---------------------------------------------------------------------
-      /// <summary>Method which gets input from user and passes to other methods which converts numerical value to roman numerals and english words(According to international numerical system)</summary>
+      /// <summary>Method which gets input from user and passes to other methods which converts numerical value to roman numerals and english words
+      /// (According to international numerical system)</summary>
       /// <param name="args"></param>
       static void Main (string[] args) {
-         long n;
-         Console.Write("Enter the value:");
+         Console.Write ("Enter the value:");
          while (true) {
-            if (long.TryParse (Console.ReadLine (), out n)) {
+            if (long.TryParse (Console.ReadLine (), out long n)) {
                Console.WriteLine ($"Original numerical value: {n}\nRoman numerals of the input\n{int_to_Roman (n)}");
                Console.WriteLine ($"Input number into words: {IntToWords (n)}");
                break;
             } else
-               Console.WriteLine ("Invalid input.Please enter an numerical value.");
+               Console.WriteLine ("Invalid input. Please enter an numerical value.");
          }
          Console.ReadKey ();
       }
-      /// <summary>This method compares the length of the input numericals and pass to differnt methods which can convert the number to equivalent english words</summary>
-      /// <param name="n"></param>
+
+      /// <summary>This method compares the length of the input numericals 
+      /// and pass to differnt methods which can convert the number to equivalent english words</summary>
+      /// <param name="n">Input number</param>
       /// <returns>It returns the string of words build by the function equivalent to the number</returns>
       public static string IntToWords (long n) {
          string[] ones = new string[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
          string[] teens = new string[] { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
          string[] tens = new string[] { "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-         string[] mult = new string[] { "zero", "Ten", "Hundred", "Thousand", "Million", "Billion", "Trillion" };
+         string[] mult = new string[] { "hundred", "thousand", "million", "billion", "trillion" };
          StringBuilder intToW = new ();
          int a = n.ToString ().Length;
          if (a >= 3)
@@ -48,63 +50,68 @@ namespace NandR {
             intToW.Append (ones[n - 1]);
          return intToW.ToString ();
       }
+
       /// <summary>Method which handle all the number which have length of more than two digits and convert it into words</summary>
-      /// <param name="n"></param>
-      /// <param name="mult"></param>
-      /// <param name="intToW"></param>
+      /// <param name="n">Input number</param>
+      /// <param name="mult">Array which has words for higher numericals</param>
+      /// <param name="intToW">Stringbuilder which holds words for number</param>
       private static void ConvertNumberToWords (long n, string[] mult, string[] ones, StringBuilder intToW) {
          switch (n) {
             case long when n >= 1000000000000:
                long trillions = n / 1000000000000;
-               intToW.Append (IntToWords (trillions)).Append (" ").Append (mult[6]).Append (" ");
+               intToW.Append (IntToWords (trillions)).Append (" ").Append (mult[4]).Append (" ");
                n %= 1000000000000;
                break;
             case long when n >= 1000000000:
                long billions = n / 1000000000;
-               intToW.Append (IntToWords (billions)).Append (" ").Append (mult[5]).Append (" ");
+               intToW.Append (IntToWords (billions)).Append (" ").Append (mult[3]).Append (" ");
                n %= 1000000000;
                break;
             case long when n >= 1000000:
                long millions = n / 1000000;
-               intToW.Append (IntToWords (millions)).Append (" ").Append (mult[4]).Append (" ");
+               intToW.Append (IntToWords (millions)).Append (" ").Append (mult[2]).Append (" ");
                n %= 1000000;
                break;
             case long when n >= 1000:
-               int thousands = (int)n / 1000;
-               intToW.Append (IntToWords (thousands)).Append (" ").Append (mult[3]).Append (" and ");
+               long thousands = n / 1000;
+               intToW.Append (IntToWords (thousands)).Append (" ").Append (mult[1]).Append (" ");
                n %= 1000;
                break;
             default:
-               int hundreds = (int)n / 100;
-               intToW.Append (ones[hundreds - 1]).Append (" ").Append (mult[2]).Append (" and ");
+               long hundreds = n / 100;
                n %= 100;
+               if (n == 0)
+                  intToW.Append (ones[hundreds - 1]).Append (" ").Append (mult[0]).Append (" ");
+               else
+                  intToW.Append (ones[hundreds - 1]).Append (" ").Append (mult[0]).Append (" and ");
                break;
          }
-         if (n > 0) {
+         if (n > 0)
             intToW.Append (IntToWords (n)).Append (" ");
-         }
       }
+
       /// <summary>This method specifically handle numbers which have only two digits and convert into words</summary>
-      /// <param name="n"></param>
-      /// <param name="ones"></param>
-      /// <param name="teens"></param>
-      /// <param name="tens"></param>
-      /// <param name="intToW"></param>
-      private static void HandleTensAndTeens (long n , string[] ones, string[] teens, string[] tens, StringBuilder intToW) {
+      /// <param name="n">Input number</param>
+      /// <param name="ones">Array which has words for unit digits</param>
+      /// <param name="teens">Array which has words for teen numbers</param>
+      /// <param name="tens">Array which has words for tens digit</param>
+      /// <param name="intToW">string builder which holds words for number</param>
+      private static void HandleTensAndTeens (long n, string[] ones, string[] teens, string[] tens, StringBuilder intToW) {
          if (n >= 20) {
             int tensDigit = (int)n / 10, onesDigit = (int)n % 10;
             intToW.Append (tens[tensDigit - 1]);
             if (onesDigit > 0)
                intToW.Append (" ").Append (ones[onesDigit - 1]);
-         } else if (n >= 10 && n < 20) {
+         } else if (n >= 10 && n < 20)
             intToW.Append (teens[n - 10]);
-         }
       }
-      /// <summary>This method converts the input number into equivalent roman numerals with the help of array of roman symbols given</summary>
-      /// <param name="n"></param>
-      /// <returns></returns>
+
+      /// <summary>Method converts the input number into equivalent roman numerals with the help of array of roman symbols given</summary>
+      /// <param name="n">Input number</param>
+      /// <returns>Returns the roman numeral of a number</returns>
       public static string int_to_Roman (long n) {
-         string[] roman_symbol = { "MMM", "MM", "M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I" };
+         string[] roman_symbol = { "MMM", "MM", "M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX", "LX",
+            "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I" };
          int[] int_value = { 3000, 2000, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
          var roman_numerals = new StringBuilder ();
          var index_num = 0;
