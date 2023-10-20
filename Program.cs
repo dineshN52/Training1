2b88c59a47b0b846d70f140bf8e5f3c78b91afdb
Dinesh N <dinesh.n@metamation.com>
2023-10-20 17:11:13 +0530

    Gitignore file excluded,Include is vlaid input method,used maxby to find maximum count

--- Program.cs
+++ Program.cs
@@ -10,43 +10,48 @@
 // Both A and B has mximum count of 3 but in sequential manner A or a attain it earlier, so A or a is winner
 // ---------------------------------------------------------------------------------------------------------------
 namespace Voting {
-   #region class Program---------------------------
+   #region class Program-----------------------
    /// <summary>Voting character with maximum count</summary>
    internal class Program {
-      #region Methods----------------------------------------------
-      /// <summary>Method gets the input from the user and find the maximum count of characters</summary>
+      #region Methods-----------------------
+      /// <summary>Method to find character with maximum repetition earlier than other characters</summary>
       /// <param name="args"></param>
       static void Main (string[] args) {
-         Console.Write ("Enter the input string:");
-         string s = Console.ReadLine ().ToLower ();
-         List<char> Dist = s.Distinct ().ToList ();
-         List<int> counts = new (Dist.Count);
-         for (int i = 0; i < Dist.Count; i++) {
-            counts.Add (0);
-            for (int j = 0; j < s.Length; j++) {
-               if (s[j] == Dist[i])
-                  counts[i] = counts[i] + 1;
-            }
-         }
-         int max = counts.Max ();
-         Vote (s, max);
-      }
-      /// <summary>Method compares count of each charcter with maximum and find the charcter which attain it first</summary>
-      /// <param name="s">Input string</param>
-      /// <param name="max">Maximu value of charcacter count array</param>
-      static void Vote (string s, int max) {
-         Dictionary<char, int> charCounts = new ();
-         for (int i = 0; i < s.Length; i++) {
-            char c = s[i];
-            if (!charCounts.ContainsKey (c))
-               charCounts[c] = 0;
-            charCounts[c]++;
-            if (charCounts[c] == max) {
-               Console.WriteLine ($"The character {c} or {c.ToString ().ToUpper ()} win\nwith the maximum count of {max} early than other characters");
-               Console.ReadKey ();
+         Console.Write ("Enter the input string: ");
+         while (true) {
+            string s = Console.ReadLine ().ToLower ();
+            if (IsValidInput (s)) {
+               List<char> Dist = s.Distinct ().ToList ();
+               List<(char Character, int Count)> charCounts = new ();
+               foreach (char c in Dist) {
+                  int count = s.Count (ch => ch == c);
+                  charCounts.Add ((c, count));
+               }
+               Vote (charCounts);
                break;
-            }
+            } else
+               Console.WriteLine ("Invalid input. Enter only alpahabetical character as input");
          }
+         Console.ReadKey ();
+      }
+
+      /// <summary>Method checks input only has alphabetical character</summary>
+      /// <param name="input">Input string</param>
+      /// <returns>Return true if all characters are alphabets,false if any is non-alphabetical character</returns>
+      static bool IsValidInput (string input) {
+         foreach (char c in input) {
+            if (!(char.IsLetter (c)))
+               return false; // If any character is not a letter, the input is invalid.
+         }
+         return true;
+      }
+
+      /// <summary>Method to find earliest charcter to attain maximum count</summary>
+      /// <param name="charCounts">List of tuple which hold characters along with their count</param>
+      static void Vote (List<(char Character, int Count)> charCounts) {
+         var (Character, Count) = charCounts.MaxBy (t => t.Count);
+         Console.WriteLine ($"Winner character is {Character.ToString ().ToUpper ()} or " +
+             Character.ToString ().ToLower () + " with count: " + Count);
       }
       #endregion
    }
