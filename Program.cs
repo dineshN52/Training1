6ea0c7571941b1b0d1a4b840c44ad6b438e49588
Dinesh N <dinesh.n@metamation.com>
2024-01-24 13:01:55 +0530

    ComplexNumber

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace Training {
          input1=input1.ComplexNum ("5+3i");
          ComplexNumber input2 = new ();
          input2 = input2.ComplexNum ("6-8i");
-         ComplexNumber output = ComplexNumber.Add (input1, input2);
-         Console.WriteLine (output);
+         ComplexNumber output1 = ComplexNumber.Add (input1, input2);
+         Console.WriteLine (output1);
       }
    }
    class ComplexNumber {
@@ -43,6 +43,7 @@ namespace Training {
       public ComplexNumber ComplexNum (string input) {
          StringBuilder real = new();
          StringBuilder imaginary = new ();
+         char SignI='+';
          for(int i=0;i<input.Length;i++) {
             char currentDigit = input[i];
             if (i+1 == input.Length) break;
@@ -54,11 +55,13 @@ namespace Training {
                case var expression when char.IsNumber (currentDigit):
                   real.Append (currentDigit);
                   break;
+               case var expression when currentDigit is '+' or '-' && input.IndexOf (currentDigit) > 0:
+                  SignI = currentDigit;
+                  break;
             }
          }
          int RealPart = int.Parse (real.ToString ());
          int ImaginaryPart = int.Parse (imaginary.ToString ());
-         char SignI = imaginary.ToString ().Contains ('-') ? '-' : '+';
          ComplexNumber n = new () {
             realPart = RealPart,
             imaginaryPart = ImaginaryPart,
@@ -68,7 +71,7 @@ namespace Training {
       }
 
       public override string ToString () {
-         return this.realPart.ToString () +this.sign+ this.imaginaryPart.ToString () + "i";
+         return this.realPart.ToString () + this.imaginaryPart.ToString () + "i";
       }
 
       int realPart { get; set; }
