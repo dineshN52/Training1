2d38e27d8cfcac5976118d2168940bd7a345fbe4
Dinesh N <dinesh.n@metamation.com>
2024-01-24 11:03:30 +0530

    Intermediate changes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,43 @@
-﻿namespace Training {
+﻿using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         var inputlist = File.ReadAllLines ("C:\\dineshn\\input.txt");
+         Dictionary<string, string[]> dict = new ();
+         foreach (string input in inputlist) {
+            string[] games = input.Split (':');
+            string[] sets = games[1].Split (';');
+            dict.Add (games[0], sets);
+            Calculate (dict);
+         }
+      }
+      static void Calculate(Dictionary <string ,string[]> dict) {
+         int red, blue, green;
+         foreach (string game in dict.Keys) {
+            string[] plays = dict[game];
+            
+            foreach(string play in plays) {
+               foreach(string set in plays) {
+                  string[] s = set.Split (',');
+                  
+                  StringBuilder b = new ();
+                  Regex r =@(^[0-12])
+                  char[] chars = s[0].ToCharArray ();
+                  foreach (char c in chars) {
+                     if (char.IsDigit (c)) {
+                        b.Append (c);
+                        
+                     }
+                  }
+               }
+               
+               
+            }
+         }
+        // string set = dict.TryGetValue (, out string[] value);
       }
    }
 }
