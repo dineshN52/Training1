572fbe1204163a50a4bea79a2707e55b9b91d1d9
Dinesh N <dinesh.n@metamation.com>
2024-01-29 18:25:02 +0530

    Final solution obtained

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Text;
+﻿using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Training {
@@ -7,13 +6,26 @@ namespace Training {
       static void Main (string[] args) {
          var inputlist = File.ReadAllLines ("C:\\dineshn\\input.txt");
          Dictionary<string, int[]> dict = new ();
+         int[] CorrectSlns = new int[100];int i = 0;
          foreach (string input in inputlist) {
             string[] games = input.Split (':');
-            string[] sets = games[1].Split (';');
-            dict.Add (games[0], CalculateRGB(sets));
+            string[] sets = games[1].Split (';');        
+            if (TryCalculateRGB(sets,out int[] f)) {
+               StringBuilder game = new ();
+               dict.Add (games[0], f);
+               char[] chars = games[0].ToCharArray ();
+               foreach (char c in chars) {
+                  if (char.IsDigit (c))
+                     game.Append (c);
+               }
+               CorrectSlns[i]=int.Parse (game.ToString());     
+            }
+            i++;
          }
+         Console.WriteLine ($"Sum of all valid games:{CorrectSlns.Sum()}");
       }
-      public static int[] CalculateRGB (string[] plays) {
+      public static bool  TryCalculateRGB (string[] plays, out int[] f) {
+         f = new int[3];
          int[] red = new int[10], blue = new int[10], green = new int[10];
          int j = 0;
          foreach (string set in plays) {                
@@ -21,10 +33,9 @@ namespace Training {
             StringBuilder b = new ();
             StringBuilder r = new ();
             StringBuilder g = new ();
-            int ag = b.Length > 0 ? int.Parse (b.ToString ()) : 0;
-            Regex re = new Regex (@"\b([0-9]|1[0-5])\s+red\b");
-            Regex gr = new Regex (@"\b([0-9]|1[0-5])\s+green\b");
-            Regex bl = new Regex (@"\b([0-9]|1[0-5])\s+blue\b");
+            Regex re = new (@"\b([0-9]|1[0-9])\s+red\b");
+            Regex gr = new (@"\b([0-9]|1[0-9])\s+green\b");
+            Regex bl = new (@"\b([0-9]|1[0-9])\s+blue\b");
             for (int i = 0; i < s.Length; i++) {            
                if (re.IsMatch (s[i])) {
                   char[] chars = s[i].ToCharArray ();
@@ -47,18 +58,22 @@ namespace Training {
                         b.Append (c);
                   }
                }
-               if (r.Length > 0)
+               if (r.Length >0)
+                  
                   red[j] = int.Parse (r.ToString ());
                if (g.Length > 0)
                   green[j] = int.Parse (g.ToString ());
                if (b.Length > 0)
                   blue[j] = int.Parse (b.ToString ());
             }
-            if()
             j++;
          }
-         int finalRed = red.Sum (), finalGreen = green.Sum (), finalBlue = blue.Sum ();
-         return new int[] { finalRed, finalGreen, finalBlue };
+         bool finalRed = !red.Any (a => a > 12), finalGreen = !green.Any (b => b > 13), finalBlue = !blue.Any (c => c > 14);
+         if(finalRed && finalGreen && finalBlue) {
+            f=new int[]{ red.Sum(),green.Sum (), blue.Sum() };
+            return true;
+         }     
+         return false;
       }
    }
 }
