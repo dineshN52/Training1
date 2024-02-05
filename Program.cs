9d2366bac3bdee5a7d5628c84ef2bf990308c9d2
Dinesh N <dinesh.n@metamation.com>
2024-02-05 17:54:51 +0530

    A10.2 double ended que

--- Program.cs
+++ Program.cs
@@ -1,7 +1,112 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+﻿// -------------------------------------------------------------------------------------------------
+// Training~A training program for new joinees at metamation,Batch-July 2023
+// Copyright(c) Metamation India
+// -------------------------------------------------------------------------------------------------
+// Program.cs
+// Creating a custom Double ended Queue
+// Includes creation of all properties like count, capacity, IsEmpty and methods like
+// HEnqueue, HDequeue(),TEnqueue, TDequeue().
+// Custom Queue method should also be able to throw exception whenever necesaary
+// Like Dequeue an element from the queue which is empty should produce Inavalidoperation Exception.
+// -------------------------------------------------------------------------------------------------
+namespace Training {
+   #region class Program-----------
+   class Program {
+      #region method---------------
+      /// <summary>Main method where the class is implemented</summary>
+      /// <param name="args"></param>
+      public static void Main () { }
+      #endregion
    }
+   #endregion
+
+   #region class TQueue----------------
+   /// <summary>Method to create a Custom Queue class</summary>
+   /// <typeparam name="T"></typeparam>
+   public class TQueue<T> {
+      public T a = default;
+      #region Private Data------
+      int mCount;
+      int mFront;
+      int mRear;
+      T[] mTQueue;
+      #endregion --------------
+
+      #region Constructor---------
+      public TQueue () {
+         mCount = 0;
+         mFront = 0;
+         mRear = 0;
+         mTQueue = new T[4];
+      }
+      #endregion
+
+      #region Properties-------------------
+      /// <summary>Method gives current number of elements in the list</summary>
+      public int Count => mCount;
+
+      /// <summary>Method gives maximum limit of elements in the list</summary>
+      public int Capacity => mTQueue.Length;
+
+      /// <summary>This method check the stack is empty or not and returns the boolen value accordingly</summary>
+      public bool IsEmpty => mCount <= 0;
+      #endregion
+
+      #region Methods--------------------
+      /// <summary>Method to enqueue a new element in the head side(Left) of queue</summary>
+      /// <param name="a">New element to be added</param>
+      public void HEnqueue (T a) {
+         if (mCount == Capacity)
+            ResizeArray ();
+         mFront = (Capacity + mFront - 1) % Capacity;
+         mTQueue[mFront] = a;
+         mCount++;
+      }
+
+      /// <summary>Method to enqueue a new element in the Tail side(left) of queue</summary>
+      /// <param name="a">New element to be added</param>
+      public void TEnqueue (T a) {
+         if (mCount == Capacity)
+            ResizeArray ();
+         mTQueue[mRear] = a;
+         mRear = (mRear + 1) % Capacity;
+         mCount++;
+      }
+
+      /// <summary>Method to Deque the element from head side of the queue</summary>
+      /// <returns>Returns the first element added on the head side</returns>
+      public T HDequeue () {
+         if (mCount > 0) {
+            a = mTQueue[mFront];
+            mFront = (mFront + 1) % Capacity;
+            mCount--;
+            return a;
+         } else
+            throw new InvalidOperationException ("Queue is empty");
+      }
+
+      /// <summary>Method to Deque the element from tail side of the queue</summary>
+      /// <returns>Returns the first element added on tail side</returns>
+      public T TDequeue () {
+         if (mCount > 0) {
+            mRear = mRear == 0 ? mCount - 1 : (mRear - 1) % Capacity;
+            a = mTQueue[mRear];
+            mCount--;
+            return a;
+         } else
+            throw new InvalidOperationException ("Queue is empty");
+      }
+
+      /// <summary>Method will double the size of que if the count exceeds the capacity</summary>
+      private void ResizeArray () {
+         for (int i = 0; i < (mCount - mFront); i++) {
+            for (int j = 0; j < Capacity - 1; j++)
+               (mTQueue[0], mTQueue[j + 1]) = (mTQueue[j + 1], mTQueue[0]);
+         }
+         Array.Resize (ref mTQueue, Capacity * 2);
+         (mFront, mRear) = (0, mCount);
+      }
+      #endregion
+   }
+   #endregion
 }
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,8 +1,47 @@
+using Training;
+
 namespace Test {
    [TestClass]
    public class UnitTest1 {
+      readonly TQueue<int> Que = new ();
       [TestMethod]
-      public void TestMethod1 () {
+      public void PropertiesTest () {
+         Assert.IsTrue (Que.IsEmpty);
+         Que.TEnqueue (1);
+         Assert.IsFalse (Que.IsEmpty);
+         Que.TEnqueue (2);
+         Que.TEnqueue (3);
+         Assert.AreEqual (3, Que.Count);
+         Que.TEnqueue (4);
+         Assert.AreEqual (4, Que.Count);
+         Assert.AreEqual (4, Que.Capacity);
+         Que.TEnqueue (5);
+         Assert.AreEqual (8, Que.Capacity);
+      }
+
+      [TestMethod]
+      public void PassTest () {
+         Que.TEnqueue (1);
+         Que.TEnqueue (2);
+         Que.TEnqueue (3);
+         Que.TEnqueue (4);
+         Assert.AreEqual (1, Que.HDequeue ());
+         Que.HEnqueue (5);
+         Assert.AreEqual (4, Que.TDequeue ());
+         Que.TEnqueue (6);
+         Assert.AreEqual (6, Que.TDequeue ());
+         Que.TEnqueue (7);
+         Que.HEnqueue (8);
+         Que.TEnqueue (9);
+         Assert.AreEqual (8, Que.HDequeue ());
+      }
+
+      [TestMethod]
+      public void Failtest () {
+         Que.HEnqueue (1);
+         Que.TDequeue ();
+         _ = Assert.ThrowsException<InvalidOperationException> (() => Que.TDequeue ());
+         _ = Assert.ThrowsException<InvalidOperationException> (() => Que.HDequeue ());
       }
    }
 }
